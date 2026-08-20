186694238a09376038ecc4d4274d9897f0b62c7e
sujal singhal <singhalsujal25@gmail.com>
2026-08-20 12:41:45 +0530

    Create HelloJenkins.java

---
+++ src/main/java/HelloJenkins.java
@@ -0,0 +1,6 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven + Jenkins!");
+        System.out.println("Build completed successfully.");
+    }
+}
